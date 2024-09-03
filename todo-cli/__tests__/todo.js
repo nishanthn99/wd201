2822de5c80ba5957a857c07a739cff91bc17c521
nishanthn99 <21e32.nishanth@sjec.ac.in>
2024-09-03 11:49:42 +0530

    level4d

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -4,6 +4,9 @@ const todoList = require("../todo");
 const { all, markAsComplete, add } = todoList();
 
 describe("Test for Todos", () => {
+  beforeEach(() => {
+    all.length = 0;
+  });
   test("should add new todo", () => {
     const TodolistCount = all.length;
     add({
@@ -14,13 +17,31 @@ describe("Test for Todos", () => {
     expect(all.length).toBe(TodolistCount + 1);
   });
   test("should mark todo as complete", () => {
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
   });
-  test("checks creating the new todo", () => {
+  test("should create a new todo", () => {
+    add({
+      title: "First Test Todo",
+      complete: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
     expect(all.length).toBe(1);
+    expect(all[0].title).toBe("First Test Todo");
+    expect(all[0].complete).toBe(false);
   });
-  test("checks marking the todo as completed", () => {
+
+  test("should mark a todo as completed", () => {
+    add({
+      title: "Submit assignment to teacher",
+      complete: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
   });
