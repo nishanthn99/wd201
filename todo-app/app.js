f249984d507b7e2dcf66eec9669a5ef5921f03da
nishanthn99 <21e32.nishanth@sjec.ac.in>
2024-09-06 08:32:01 +0530

    level8

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,34 +1,60 @@
+/* eslint-disable no-undef */
 const express = require("express");
+var csrf=require("tiny-csrf");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
-const path = require("path");
+var cookieParser = require("cookie-parser")
+const path =require("path");
+
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser("shh! some secret string"));
+app.use(csrf("this_should_be_32_character_long",["POST","PUT","DELETE"]));
 
-// eslint-disable-next-line no-undef
-app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
-
-//Todo.createTodo({title:"first todo",dueDate:"2024-10-10"});
-//Todo.createTodo({title:"sec todo",dueDate:"2023-09-09"});
-
-app.get("/", async function (request, response) {
-  const getTodos = await Todo.getTodos();
+app.use(express.static(path.join(__dirname,'public')));
+app.get("/", async (request, response) => {
+  const Overdue = await Todo.getOverdueTodos();
+  const dueToday= await Todo.getdueTodayTodos();
+  const dueLater= await Todo.getdueLaterTodos();
+  const Completed = await Todo.completedItems();
   if (request.accepts("html")) {
-    response.render("index", { getTodos });
+    response.render('index', {
+      Overdue,
+      dueToday,
+      dueLater,
+      Completed,
+      csrfToken: request.csrfToken(),
+    });
   } else {
-    response.json(getTodos);
+    response.json({
+      Overdue,
+      dueToday,
+      dueLater,
+      Completed
+    });
   }
+});
+
+app.get("/", function (request, response) {
+  response.send("Hello World");
 });
 
 app.get("/todos", async function (_request, response) {
   console.log("Processing list of all Todos ...");
-  try {
-    const TodoItems = await Todo.findAll();
-    response.json(TodoItems);
-  } catch (error) {
-    response.status(455).json(error);
+  // FILL IN YOUR CODE HERE
+
+  // First, we have to query our PostgerSQL database using Sequelize to get list of all Todos.
+  // Then, we have to respond with all Todos, like:
+  try{
+    const todos =await Todo.findAll();
+    return response.send(todos);
+  }catch(error){
+    console.log(error);
+    return response.status(422).json(error);
   }
+  // response.send(todos)
 });
 
 app.get("/todos/:id", async function (request, response) {
@@ -43,8 +69,8 @@ app.get("/todos/:id", async function (request, response) {
 
 app.post("/todos", async function (request, response) {
   try {
-    const todo = await Todo.createTodo(request.body);
-    return response.json(todo);
+    await Todo.addTodo(request.body);
+    return response.redirect("/");
   } catch (error) {
     console.log(error);
     return response.status(422).json(error);
@@ -64,16 +90,24 @@ app.put("/todos/:id/markAsCompleted", async function (request, response) {
 
 app.delete("/todos/:id", async function (request, response) {
   console.log("We have to delete a Todo with ID: ", request.params.id);
-  const id = request.params.id;
-  console.log(`deleting todo for id${id}`);
-  try {
-    const todo = await Todo.findByPk(id);
-    await todo.destroy();
-    response.json(true);
-  } catch (err) {
-    response.status(500).json(false);
-    console.log(err);
+  // FILL IN YOUR CODE HERE
+  if (await Todo.findByPk(request.params.id)) {
+    await Todo.destroy({
+      where: {
+        id: request.params.id,
+      },
+    });
+    if (await Todo.findByPk(request.params.id)) {
+      response.send(false);
+    } else {
+      response.send(true);
+    }
+  } else {
+    response.send(false);
   }
+  // First, we have to query our database to delete a Todo by ID.
+  // Then, we have to respond back with true/false based on whether the Todo was deleted or not.
+  // response.send(true)
 });
 
 module.exports = app;
--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-unused-vars */
-"use strict";
-const { Model } = require("sequelize");
+'use strict';
+const {
+  Model,
+  Op
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
@@ -11,28 +14,123 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-    static async createTodo({ title, dueDate }) {
-      return this.create({ title: title, dueDate: dueDate, completed: false });
+    static addTodo({title,dueDate}){
+      return this.create({title: title, dueDate: dueDate, completed: false});
     }
-
-    static getTodos() {
+    static async getTodos(){
       return this.findAll();
     }
-
     markAsCompleted() {
-      return this.update({ completed: true });
+      return this.update({completed: true});
+    }
+    static async getOverdueTodos(){
+      try{
+        const OverdueTodos =await Todo.findAll({
+          where: {
+            dueDate:{
+              [Op.lt]: new Date(),
+            },
+          },
+        });
+        if(OverdueTodos.length >= 1){
+          return OverdueTodos
+        }else{
+          await this.addTodo({
+            title:"Go to Home",
+            dueDate: new Date(new Date().setDate(new Date().getDate()-1)).toISOString(),
+            completed:false,
+          });          
+        }
+        const Overdue =await this.getOverdueTodos;
+        return Overdue
+      }catch(error) {
+        console.error('Error!!!',error);
+        throw error;
+      }
+    }
+    static async getdueTodayTodos(){
+      try{
+        const DueTodayTodos =await Todo.findAll({
+          where: {
+            dueDate:{
+              [Op.between]:[new Date(),new Date(new Date().setHours(23,59,59,999))],
+            },
+          },
+        });
+        if(DueTodayTodos.length >=1){
+          return DueTodayTodos;
+        }else{
+          await this.addTodo({
+            title:"Buy milk",
+            dueDate: new Date().toISOString(),
+            completed:false,
+          });
+        }
+        const dueToday = this.getdueTodayTodos;
+        return dueToday
+      }catch(error) {
+        console.error('Error!!!',error);
+        throw error;
+      }
+    }
+    static async getdueLaterTodos(){
+      try{
+        const DueLaterTodos =await Todo.findAll({
+          where: {
+            dueDate:{
+              [Op.gt]: new Date(),
+            },
+          },
+        });
+        if(DueLaterTodos.length >=1){
+          return DueLaterTodos;
+        }else{
+          const tomorrow = new Date();
+          tomorrow.setDate(tomorrow.getDate() + 1);
+          await this.addTodo({
+            title:"Have to pay electricity bill",
+            dueDate:  tomorrow.toISOString(),
+            completed:false,
+          });
+        }
+        const dueLater = await this.getdueLaterTodos;
+        return dueLater
+      }catch(error) {
+        console.error('Error!!!',error);
+        throw error;
+      }
+    }
+    static async remove(id) {
+      return this.destroy({
+        where:{
+          id,
+        }
+      })
+    }
+    static async completedItems(){
+      try{
+        const Completed = await this.findAll({
+          where:{
+          completed: true,
+        }
+      })
+      return Completed
+    }catch(error){
+      console.error('Error!!!' , error);
+      throw error;
     }
   }
-  Todo.init(
-    {
-      title: DataTypes.STRING,
-      dueDate: DataTypes.DATEONLY,
-      completed: DataTypes.BOOLEAN,
-    },
-    {
-      sequelize,
-      modelName: "Todo",
-    },
-  );
+  setCompletionStatus(receiver){
+    return this.update({completed: receiver});
+  }
+}
+  Todo.init({
+    title: DataTypes.STRING,
+    dueDate: DataTypes.DATEONLY,
+    completed: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Todo',
+  });
   return Todo;
 };
